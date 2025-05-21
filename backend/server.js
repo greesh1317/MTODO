52ae049f6ed9670351d686e363c3da93fa65205b
solasa greeshma divya sree <solasagreeshmadivyasree@gmail.com>
2025-05-21 14:46:40 +0530

    list controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
-const{errorHandler} = require('./middleware/errorMiddleware')
-const port = process.env.PORT || 5000;
+const{errorHandler} = require('./middleware/errorMiddleware');
 
-const app = express();
+const port = process.env.PORT || 5000
+
+
+
+const app = express()
 
 app.use(express.json())
+
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/list', require('./routes/listRoutes'))
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`))
